1cd4b82868b1c0b9887b262b2741d8c3953af19f
0nyinye <onyinyeekezi2017@gmail.com>
2025-07-11 14:31:51 +0100

    created logout dto

---
+++ FraudGuard/src/main/java/com/fraudguard/fraudguard/dto/response/LogoutResponse.java
@@ -0,0 +1,14 @@
+package com.fraudguard.fraudguard.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LogoutResponse {
+
+    private String message;
+
+}
