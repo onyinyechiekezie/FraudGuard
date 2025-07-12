5d7278ec70f905bf16253c8ac5c47541ab574c98
Olubiyitaiwo <olubiyipaul01@gmail.com>
2025-07-12 22:53:44 +0100

    changes to agent service

--- FraudGuard/src/main/java/com/fraudguard/fraudguard/services/AgentService.java
+++ FraudGuard/src/main/java/com/fraudguard/fraudguard/services/AgentService.java
@@ -1,11 +1,16 @@
 package com.fraudguard.fraudguard.services;
 
 import com.fraudguard.fraudguard.data.models.Agent;
+import com.fraudguard.fraudguard.data.models.Notification;
 
 import java.util.List;
 
 public interface AgentService {
     Agent getAgentById(String id);
+
     List<Agent> getAllAgents();
+
     Agent saveAgent(Agent agent);
+
+    Notification viewNotification(String agent);
 }
